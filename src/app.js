8b3624f303630f88c1b0151e88038e426bc0422a
Kennith Leung <kennith.leung@gmail.com>
2020-04-16 20:08:32 -0700

    Refactor URL

--- src/app.js
+++ src/app.js
@@ -18,33 +18,33 @@ let app = new Vue({
 	},
 	methods: {
 		getLACount: function() {
-			axios.get('https://corona.lmao.ninja/v2/jhucsse/counties')
+			axios.get(this.url + '/jhucsse/counties/Los Angeles')
 				 .then(response => (this.la = _.find(response.data, function(data) { return data.county == "Los Angeles"; } ).stats))
 				 .catch(error => console.log(error));
 		},
 		getStates: function() {
-			axios.get('https://corona.lmao.ninja/v2/countries/usa')
+			axios.get(this.url + '/countries/usa')
 				.then(response => (this.usa = response.data))
 				.catch(error => console.log(error))
 		},
 		getCaCount: function() {
-			axios.get('https://corona.lmao.ninja/v2/states')
+			axios.get(this.url + '/states')
 				.then(response => (this.ca = this.parseCaCount(response.data)))
 				.catch(error => console.log(error))
 
 		},
 		getHkCount: function() {
-			axios.get('https://corona.lmao.ninja/v2/countries/Hong%20Kong')
+			axios.get(this.url + '/countries/Hong%20Kong')
 				.then(response => (this.hk = this.parseLamoCount(response.data)))
 				.catch(error => console.log(error))
 		},
 		getItalyCount: function() {
-			axios.get('https://corona.lmao.ninja/v2/countries/Italy')
+			axios.get(this.url + '/countries/Italy')
 				.then(response => (this.italy = this.parseLamoCount(response.data)))
 				.catch(error => console.log(error))
 		},
 		getJapanCount: function() {
-			axios.get('https://corona.lmao.ninja/v2/countries/Japan')
+			axios.get(this.url + '/countries/Japan')
 				.then(response => (this.japan = this.parseLamoCount(response.data)))
 				.catch(error => console.log(error))
 		},
@@ -71,6 +71,7 @@ let app = new Vue({
 		japan: {},
 		la: {},
 		jhuData: '',
+		url: 'https://corona.lmao.ninja/v2',
 	},
 	filters: {
 		formatNumber: function(value) {
